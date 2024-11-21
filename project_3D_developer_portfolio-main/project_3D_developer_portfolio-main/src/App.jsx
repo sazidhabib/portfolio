6dc28e7e2d899903f1977f05940bd0200e37b0ab
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2024-11-21 19:17:10 +0600

    11 novber update.

--- project_3D_developer_portfolio-main/project_3D_developer_portfolio-main/src/App.jsx
+++ project_3D_developer_portfolio-main/project_3D_developer_portfolio-main/src/App.jsx
@@ -1,27 +1,42 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import HeroNew from "./components/HeroNew";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
+
         <About />
         <Experience />
+
         <Tech />
-        <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
+        <div className="relative z-0">
+          <Works />
+          <Feedbacks />
+
           <Contact />
           <StarsCanvas />
         </div>
+        <HeroNew />
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ project_3D_developer_portfolio-main/project_3D_developer_portfolio-main/src/components/HeroNew.jsx
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+
+const HeroNew = () => {
+  return (
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <section className={`relative w-full h-screen mx-auto`}>
+          <div
+            className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+          >
+            <div className="flex flex-col justify-center items-center mt-5">
+              <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+              <div className="w-1 sm:h-80 h-40 violet-gradient" />
+            </div>
+
+            <div>
+              <h1 className={`${styles.heroHeadText} text-white`}>
+                Hi, I'm <span className="text-[#915EFF]">Adrian</span>
+              </h1>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                I develop 3D visuals, user <br className="sm:block hidden" />
+                interfaces and web applications
+              </p>
+            </div>
+          </div>
+
+          <motion.div
+            animate={{
+              y: [0, 24, 0],
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+            className="w-3 h-3 rounded-full bg-secondary mb-1"
+          >
+            <img src="/planet/chareacter.png" alt="Logo" />
+          </motion.div>
+          {/* <ComputersCanvas /> */}
+
+          <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+            <a href="#about">
+              <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                <motion.div
+                  animate={{
+                    y: [0, 24, 0],
+                  }}
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                  }}
+                  className="w-3 h-3 rounded-full bg-secondary mb-1"
+                />
+              </div>
+            </a>
+          </div>
+        </section>
+      </motion.div>
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto"
+      >
+        <img src="/planet/chareacter.png" alt="Logo" />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(HeroNew, "contact");
